8e1202c7a156a9e47ad7c7943b24e57309d4b30f
Maihassan56 <mai.hassan4528@gmail.com>
2024-06-04 17:41:36 +0300

    rdr

---
+++ Demo.BLL/Interfaces/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAcess.Repository.IRepository
+{
+    public interface IOrderDetailRepository
+    {
+        void Update(Models.OrderDetail orderDetail);
+    }
+}
